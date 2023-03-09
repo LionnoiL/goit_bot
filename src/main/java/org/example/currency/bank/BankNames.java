d5b76cd4b93554524b4089641cc58850c06167fa
TarasProrok <170684@ukr.net>
2023-03-09 13:05:56 +0200

    Interface and method for UA bankNames

---
+++ src/main/java/org/example/currency/bank/BankNames.java
@@ -0,0 +1,5 @@
+package org.example.currency.bank;
+
+public interface BankNames {
+    public String getBankName (Bank bank);
+}
---
+++ src/main/java/org/example/currency/bank/UaBankNames.java
@@ -0,0 +1,16 @@
+package org.example.currency.bank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UaBankNames {
+    private static Map<Bank, String> bankNames = new HashMap<>();
+    static {
+        bankNames.put(Bank.MONOBANK, "Монобанк");
+        bankNames.put(Bank.PRIVATBANK, "Приватбанк");
+        bankNames.put(Bank.NBU, "НБУ");
+    }
+    public String getBankName(Bank bank) {
+        return bankNames.get(bank);
+    }
+}
