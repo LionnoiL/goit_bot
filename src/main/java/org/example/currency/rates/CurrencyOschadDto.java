fd2a8f5c0c4a31b442c31d02cc509ca0a7eb71aa
Oleksii Podhurskyi <podhurskyi.aleks@gmail.com>
2023-03-11 13:02:07 +0200

    Added CurrencyOschadDto and CurrencyRetrievalOschadService

--- src/main/java/org/example/currency/rates/CurrencyOschadDto.java
+++ src/main/java/org/example/currency/rates/CurrencyOschadDto.java
@@ -1,2 +1,20 @@
-package org.example.currency.rates;public class CurrencyOschadDto {
+package org.example.currency.rates;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.currency.bank.Bank;
+import org.example.currency.currencies.Currency;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyOschadDto {
+    private Currency currency;
+    private BigDecimal buyRate;
+    private BigDecimal sellRate;
+    private Bank bankName;
 }
+
--- src/main/java/org/example/currency/services/CurrencyRetrievalOschadService.java
+++ src/main/java/org/example/currency/services/CurrencyRetrievalOschadService.java
@@ -1,2 +1,42 @@
-package org.example.currency.services;public class CurrencyRetrievalOschadService {
+package org.example.currency.services;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.stream.Collectors;
+
+public class CurrencyRetrievalOschadService implements CurrencyRetrievalService {
+
+    private static final String URL = "https://www.oschadbank.ua/currency-rate";
+
+    @Override
+    public List<CurrencyRateDto> getCurrencyRates() {
+
+        try {
+            Document document = Jsoup.connect(URL).get();
+            Element currencyRateTable = document.getElementsByTag("tbody").get(1);
+            Elements tableRows = currencyRateTable.getElementsByTag("tr");
+
+            return tableRows.stream()
+                    .filter(element -> element.select("td:nth-child(2)").text().equals("USD")
+                            || element.select("td:nth-child(2)").text().equals("EUR"))
+                    .map(item -> {
+                        CurrencyOschadDto dto = new CurrencyOschadDto();
+                        dto.setCurrency(Currency.valueOf(item.select("td:nth-child(2)").text()));
+                        dto.setBuyRate(new BigDecimal(item.select("td:nth-child(4)").text().replaceFirst(",", ".")));
+                        dto.setSellRate(new BigDecimal(item.select("td:nth-child(5)").text().replaceFirst(",", ".")));
+                        dto.setBankName(Bank.OSCHADBANK);
+                        return dto;
+                    })
+                    .map(item -> new CurrencyRateDto(item.getCurrency(), item.getBuyRate(), item.getSellRate(), item.getBankName()))
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
