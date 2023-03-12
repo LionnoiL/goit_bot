ed4c13cc163d43451c6c417097ffdf53860a995a
bit-to-bit <vasiliy.pykhteev@gmail.com>
2023-03-12 18:28:50 +0200

    Додав метод FilesUtils.checkFileDirAndCreateDir. При збереженні файлу перевіряє, чи існує каталог, якщо ні, то створює його.

--- src/main/java/org/example/utils/FilesUtils.java
+++ src/main/java/org/example/utils/FilesUtils.java
@@ -1,28 +1,35 @@
 package org.example.utils;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FilesUtils {
-
-  private FilesUtils() {
-  }
-
-  public static void saveTextFile(String filePath, String text) {
-    FileWriter file = null;
-    try {
-      file = new FileWriter(filePath);
-      file.write(text);
-    } catch (IOException e) {
-      System.out.println("Error save file! " + filePath);
+    private FilesUtils() {
     }
 
-    if (file != null) {
-      try {
-        file.close();
-      } catch (IOException e) {
-        System.out.println("Error close file! " + filePath);
-      }
+    public static void saveTextFile(String filePath, String text) {
+        FileWriter file = null;
+        try {
+            checkFileDirAndCreateDir(filePath);
+            file = new FileWriter(filePath);
+            file.write(text);
+        } catch (IOException e) {
+            System.out.println("Error save file! " + filePath);
+        }
+        if (file != null) {
+            try {
+                file.close();
+            } catch (IOException e) {
+                System.out.println("Error close file! " + filePath);
+            }
+        }
     }
-  }
+
+    public static void checkFileDirAndCreateDir(String filePath) throws IOException {
+        File dir = new File(new File(filePath).getParent());
+        if (!dir.exists() && !dir.mkdirs()) {
+            throw new IOException("Error create dir! " + dir.getAbsolutePath());
+        }
+    }
 }
