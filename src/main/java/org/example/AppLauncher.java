f9e64670bb811b511edf82f565a68122c700381f
gaponov <wmconstructor@gmail.com>
2023-03-12 22:12:45 +0200

    add README.md

--- src/main/java/org/example/AppLauncher.java
+++ src/main/java/org/example/AppLauncher.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.notifications.Scheduler;
 import org.example.properties.ApplicationProperties;
 import org.example.telegrambots.currency.CurrencyBot;
@@ -10,14 +12,15 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class AppLauncher {
 
     public static final ApplicationProperties APPLICATION_PROPERTIES = new ApplicationProperties();
+    private static final Logger LOG = LogManager.getLogger(AppLauncher.class);
 
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new CurrencyBot());
-            // botsApi.registerBot(new WeatherBot());
+            //botsApi.registerBot(new WeatherBot());
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            LOG.warn("Error register bots");
         }
         Scheduler.setTimeReceived();
     }
