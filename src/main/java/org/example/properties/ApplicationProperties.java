b3a06bbb44f485b1acbe402f204131f09eb2c658
Andriy Gaponov <wmconstructor@gmail.com>
2023-03-13 09:34:02 +0200

    add log4j-slf4j2-impl in gradle. fix get default values in ApplicationProperties

--- src/main/java/org/example/properties/ApplicationProperties.java
+++ src/main/java/org/example/properties/ApplicationProperties.java
@@ -19,76 +19,70 @@ import org.example.utils.FilesUtils;
 @Getter
 public class ApplicationProperties {
 
-    public static final String CACHE_PATH = "cache/";
-    static final String RESOURCES_PATH = "";
-    static final String APPLICATION_PROPERTIES_FILE_NAME = "config/application.properties";
-    static final String JSON_USERS_FILE_NAME = "db/users.json";
-    private static final Logger LOG = LogManager.getLogger(ApplicationProperties.class);
-    private int decimalPrecision;
-    private Bank bank;
-    private Currency currency;
+  public static final String CACHE_PATH = "cache/";
+  static final String RESOURCES_PATH = "";
+  static final String APPLICATION_PROPERTIES_FILE_NAME = "config/application.properties";
+  static final String JSON_USERS_FILE_NAME = "db/users.json";
+  private static final Logger LOG = LogManager.getLogger(ApplicationProperties.class);
+  private int decimalPrecision;
+  private Bank bank;
+  private Currency currency;
 
-    @Setter
-    private Map<Long, User> users;
+  @Setter
+  private Map<Long, User> users;
 
-    public ApplicationProperties() {
-        loadFromFile();
-        users = getUsersListFromFile();
+  public ApplicationProperties() {
+    loadFromFile();
+    users = getUsersListFromFile();
+  }
+
+  public static void saveUsersListToFile() {
+    FilesUtils.saveTextFile(RESOURCES_PATH + JSON_USERS_FILE_NAME,
+        new Gson().toJson(AppLauncher.APPLICATION_PROPERTIES.getUsers()));
+  }
+
+  private void loadFromFile() {
+    String decimalPrecisionString = PropertiesService.getApplicationProperties(
+        APPLICATION_PROPERTIES_FILE_NAME,
+        "decimalPrecision");
+    String bankString = PropertiesService.getApplicationProperties(
+        APPLICATION_PROPERTIES_FILE_NAME, "bank");
+    String currencyString = PropertiesService.getApplicationProperties(
+        APPLICATION_PROPERTIES_FILE_NAME, "currency");
+
+    if (!decimalPrecisionString.isBlank()) {
+      decimalPrecision = Integer.parseInt(decimalPrecisionString);
+    } else {
+      decimalPrecision = 2;
     }
 
-    public static void saveUsersListToFile() {
-        FilesUtils.saveTextFile(RESOURCES_PATH + JSON_USERS_FILE_NAME,
-            new Gson().toJson(AppLauncher.APPLICATION_PROPERTIES.getUsers()));
+    if (!bankString.isBlank()) {
+      bank = Bank.valueOf(bankString);
+    } else {
+      bank = Bank.NBU;
     }
 
-    private void loadFromFile() {
-        String decimalPrecisionString = PropertiesService.getApplicationProperties(
-            APPLICATION_PROPERTIES_FILE_NAME,
-            "decimalPrecision");
-        String bankString = PropertiesService.getApplicationProperties(
-            APPLICATION_PROPERTIES_FILE_NAME, "bank");
-        String currencyString = PropertiesService.getApplicationProperties(
-            APPLICATION_PROPERTIES_FILE_NAME, "currency");
+    if (!currencyString.isBlank()) {
+      currency = Currency.valueOf(currencyString);
+    } else {
+      currency = Currency.USD;
+    }
+  }
 
-        try {
-            if (!decimalPrecisionString.isBlank()) {
-                decimalPrecision = Integer.parseInt(decimalPrecisionString);
-            }
-        } finally {
-            decimalPrecision = 2;
-        }
-
-        try {
-            if (!bankString.isBlank()) {
-                bank = Bank.valueOf(bankString);
-            }
-        } finally {
-            bank = Bank.NBU;
-        }
-
-        try {
-            if (!currencyString.isBlank()) {
-                currency = Currency.valueOf(currencyString);
-            }
-        } finally {
-            currency = Currency.USD;
-        }
+  public Map<Long, User> getUsersListFromFile() {
+    Map<Long, User> savedUsers = new HashMap<>();
+    String jsonFileName = RESOURCES_PATH + JSON_USERS_FILE_NAME;
+    File file = new File(jsonFileName);
+    if (!file.exists()) {
+      FilesUtils.saveTextFile(jsonFileName, "{}");
     }
 
-    public Map<Long, User> getUsersListFromFile() {
-        Map<Long, User> savedUsers = new HashMap<>();
-        String jsonFileName = RESOURCES_PATH + JSON_USERS_FILE_NAME;
-        File file = new File(jsonFileName);
-        if (!file.exists()) {
-            FilesUtils.saveTextFile(jsonFileName, "{}");
-        }
-
-        try (FileReader fileReader = new FileReader(jsonFileName)) {
-            savedUsers = new Gson().fromJson(fileReader, new TypeToken<Map<Long, User>>() {
-            }.getType());
-        } catch (Exception e) {
-            LOG.warn("Error read users file from " + JSON_USERS_FILE_NAME);
-        }
-        return savedUsers;
+    try (FileReader fileReader = new FileReader(jsonFileName)) {
+      savedUsers = new Gson().fromJson(fileReader, new TypeToken<Map<Long, User>>() {
+      }.getType());
+    } catch (Exception e) {
+      LOG.warn("Error read users file from " + JSON_USERS_FILE_NAME);
     }
+    return savedUsers;
+  }
 }
