f8934b0df3db709ab403e25aaa47e63129098126
Andriy Gaponov <wmconstructor@gmail.com>
2024-01-17 09:33:27 +0200

    fix privatbank ranges

--- src/main/java/ua/dpw/currency/services/CurrencyRetrievalPrivatService.java
+++ src/main/java/ua/dpw/currency/services/CurrencyRetrievalPrivatService.java
@@ -1,17 +1,18 @@
 package ua.dpw.currency.services;
 
-import static ua.dpw.database.Service.BANK_SERVICE;
-import static ua.dpw.database.Service.CURRENCY_SERVICE;
-
 import com.google.gson.JsonObject;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import ua.dpw.currency.bank.Bank;
 import ua.dpw.currency.rates.CurrencyRate;
 import ua.dpw.utils.JsonConverter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ua.dpw.database.Service.BANK_SERVICE;
+import static ua.dpw.database.Service.CURRENCY_SERVICE;
 
 @Slf4j
 public class CurrencyRetrievalPrivatService implements CurrencyRetrievalService {
@@ -26,16 +27,16 @@ public class CurrencyRetrievalPrivatService implements CurrencyRetrievalService 
         try {
             String response = Jsoup.connect(URL).ignoreContentType(true).get().body().text();
             List<JsonObject> responseJson = JsonConverter.convertJsonStringToList(
-                response, JsonObject.class);
+                    response, JsonObject.class);
 
             result = responseJson.stream()
-                .map(dto -> new CurrencyRate(
-                    BANK,
-                    CURRENCY_SERVICE.getByName(dto.get("ccy").getAsString()),
-                    dto.get("buy").getAsBigDecimal(),
-                    dto.get("sale").getAsBigDecimal()
-                ))
-                .toList();
+                    .map(dto -> new CurrencyRate(
+                            BANK,
+                            CURRENCY_SERVICE.getByName(dto.get("ccy").getAsString()),
+                            dto.get("sale").getAsBigDecimal(),
+                            dto.get("buy").getAsBigDecimal()
+                    ))
+                    .toList();
         } catch (IOException e) {
             log.error("Error get rates from Privatbank api");
         }
